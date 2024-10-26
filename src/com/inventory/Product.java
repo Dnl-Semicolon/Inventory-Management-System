7e237a4c81992b4d9579f9525f396a68f5d93cd2
Daniel Yee Kheng Tan <danielykt-pm23@student.tarc.edu.my>
2024-10-26 17:10:37 +0800

    gotten the basic methods and SimpleProduct now implements Product

--- src/com/inventory/Product.java
+++ src/com/inventory/Product.java
@@ -1,4 +1,8 @@
 package com.inventory;
 
 public interface Product {
+	int getId();
+	String getName();
+	int getQuantity();
+	double getPrice();
 }
--- src/com/inventory/SimpleProduct.java
+++ src/com/inventory/SimpleProduct.java
@@ -1,4 +1,23 @@
 package com.inventory;
 
-public class SimpleProduct {
+public class SimpleProduct implements Product {
+	@Override
+	public int getId() {
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		return "";
+	}
+
+	@Override
+	public int getQuantity() {
+		return 0;
+	}
+
+	@Override
+	public double getPrice() {
+		return 0;
+	}
 }
